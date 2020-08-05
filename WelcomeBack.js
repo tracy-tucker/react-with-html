2854dfa3439ca835879b57492e59abcc9a63d85e
treuther <tracyreuther@gmail.com>
2020-08-05 04:52:16 -0500

    style added

--- WelcomeBack.js
+++ WelcomeBack.js
@@ -1,16 +1,25 @@
 class WelcomeBack extends React.Component {
-    constructor() {
+    constructor(props){
+        super(props);
         this.state = {
             name: 'J',
-            appVersion: '1'
+            apppVersion: '1'
         }
     }
 
-    render() {
-        return (
+    render(){
+        return(
             <>
                 <h2>Hello {this.state.name || 'Friend'}! Welcome Back.</h2>
+                {
+                    this.state.apppVersion && this.state.apppVersion < 2
+                    ? <p>Your app is out of date. Please download the new version and take a look at all the new features.</p> 
+                    : ''
+                }
+                <CoolButton customText={this.state.apppVersion && this.state.apppVersion < 2 ? 'Download v2' : 'Download'} />
             </>
         )
     }
+
+
 }
